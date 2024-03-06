7ef154e2ec699b2c46269cbd528104c4988191a5
SweetieWind <100509108+SweetieWind@users.noreply.github.com>
2024-03-05 22:09:34 -0600

    Collision fix, restart aadd
    añadi la colision que quita vida, cambie los valores a 10 y añadi que se ponga rojo cuando muere

--- Assets/Dante/Scripts/PlayerManager.cs
+++ Assets/Dante/Scripts/PlayerManager.cs
@@ -4,35 +4,79 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+
+    public bool isAlive = true;
     [SerializeField]
-    private float playerlife = 100;
+    private int healthPoints = 10;
+    private float healthTimer = 0.5f;
     [SerializeField]
-    private float lifeTimer = 0.5f;
+    private int layerInt;
+    SpriteRenderer sprite;
     [SerializeField]
-    private int layerTrap = 7;
+    private Transform respawnPosition;
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R) && isAlive == false)
+        {
+            RestartPlayer();
+        }
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == layerTrap)
+        if (collision.gameObject.layer == layerInt)
         {
-            if(lifeTimer > 0)
+            if (healthTimer > 0)
             {
-                lifeTimer -= Time.deltaTime;
+                healthTimer -= Time.deltaTime;
             }
-            if (lifeTimer <= 0)
+            else if (healthTimer <= 0 && isAlive)
             {
-                playerlife--;
-                lifeTimer = 0.5f;
+                healthPoints -= 1;
+                if (healthPoints <= 0)
+                {
+                    isAlive = false;
+                    Debug.Log("Perdiste");
+                    playerLose();
+                }
+                healthTimer = 0.5f;
             }
         }
+        if (collision.gameObject.tag == "Heal")
+        {
+            if (healthTimer > 0)
+            {
+                healthTimer -= Time.deltaTime;
+            }
+            else if (healthTimer < 9 && isAlive)
+            {
+                healthPoints += 1;
+                healthTimer = 0.5f;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Win")
+        {
+            Debug.Log("Ganaste");
+        }
+    }
+    private void playerLose()
+    {
+        sprite.color = Color.red;
+    }
+    public void RestartPlayer()
+    {
+        gameObject.transform.position = respawnPosition.position;
+        sprite.color = Color.white;
+        healthPoints = 10;
+        isAlive = true;
     }
 }
